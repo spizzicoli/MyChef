9e07b9794aab422f3d73e7ce3dfa8893289d3157
Samuele <samuelepizzicoli@gmail.com>
2021-02-25 13:53:27 +0100

    removed-barcodescanner-plugins

--- src/pages/Pantry.tsx
+++ src/pages/Pantry.tsx
@@ -2,33 +2,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBa
 import React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Plugins } from '@capacitor/core'
-import {
-  BarcodeScannerPlugin,
-  EVENT_BARCODE_SCANNED,
-  IBarcodeScannerEventPayload,
-} from 'capacitor-plugin-barcodescanner'
 
 const User: React.FC<RouteComponentProps> = ({ location }) => {
   // state = location.state as { name: string }
-
-  // can be used later to remove BarcodeScanner listener
-  let removeScannerHandler: () => void;
-   
-  const barcodeScanned = (state: IBarcodeScannerEventPayload): void => {
-    console.log("BarcodeScanned - ",state.cancelled, state.barcode, state.error)
-  }
-
-  const BarcodeScanner = Plugins.BarcodeScanner as BarcodeScannerPlugin;
-  removeScannerHandler = BarcodeScanner.addListener(
-    EVENT_BARCODE_SCANNED,
-    barcodeScanned
-  ).remove;
- 
-  BarcodeScanner.scan();
-
-  if(removeScannerHandler) {
-    removeScannerHandler();
-  }
 
   return (
     <IonPage>
